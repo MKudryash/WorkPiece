a33fab94669123fe725df4b96c2651f5bb60c82d
Julia Mamsheva <Jukudryash@gmail.com>
2022-09-28 17:16:35 +0300

    This work

--- app/src/main/java/com/example/workpiece/MainActivity.java
+++ app/src/main/java/com/example/workpiece/MainActivity.java
@@ -4,14 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     View v;
+    Connection connection;
+    List<Mask> data;
+    ListView listView;
+    AdapterMask pAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         v = findViewById(com.google.android.material.R.id.ghost_view);
+
+        GetTextFromSQL(v);
+    }
+    public void enterMobile() {
+        pAdapter.notifyDataSetInvalidated();
+        listView.setAdapter(pAdapter);
+    }
+    public void GetTextFromSQL(View v) {
+        data = new ArrayList<Mask>();
+        listView = findViewById(R.id.lvData);
+        pAdapter = new AdapterMask(MainActivity.this, data);
+        try {
+            ConnectionHelper connectionHelper = new ConnectionHelper();
+            connection = connectionHelper.connectionClass();
+            if (connection != null) {
+                String query = "Select * From Masks";
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+
+                while (resultSet.next()) {
+                    Mask tempMask = new Mask
+                            (resultSet.getInt("ID"),
+                                    resultSet.getString("Title"),
+                                    Integer.parseInt(resultSet.getString("Count")),
+                                    resultSet.getString("Image")
+                            );
+                    data.add(tempMask);
+                    pAdapter.notifyDataSetInvalidated();
+                }
+                connection.close();
+            } else {
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        enterMobile();
+
     }
 }
---
+++ app/src/main/java/com/example/workpiece/Mask.java
@@ -0,0 +1,83 @@
+package com.example.workpiece;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Mask implements Parcelable {
+
+    private int ID;
+    private String Title;
+    private int Count;
+    private String Image;
+
+    protected Mask(Parcel in) {
+        ID = in.readInt();
+        Title = in.readString();
+        Count = in.readInt();
+        Image = in.readString();
+    }
+
+    public static final Creator<Mask> CREATOR = new Creator<Mask>() {
+        @Override
+        public Mask createFromParcel(Parcel in) {
+            return new Mask(in);
+        }
+
+        @Override
+        public Mask[] newArray(int size) {
+            return new Mask[size];
+        }
+    };
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public void setCount(int count) {
+        Count = count;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public int getCount() {
+        return Count;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public Mask(int ID, String title, int count, String image) {
+        this.ID = ID;
+        Title = title;
+        Count = count;
+        Image = image;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(ID);
+        dest.writeString(Title);
+        dest.writeInt(Count);
+        dest.writeString(Image);
+    }
+}
